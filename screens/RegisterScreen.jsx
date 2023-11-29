295087eb9cd03718bbaf022b2d7a9ec56119d489
yukselozt <ykslztrk9@gmail.com>
2023-11-29 16:52:26 +0300

    (+)Add formik & yup into Register Screen

--- screens/RegisterScreen.jsx
+++ screens/RegisterScreen.jsx
@@ -6,9 +6,10 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import * as Yup from "yup";
 import { Formik } from "formik";
+import { Dropdown } from "react-native-element-dropdown";
 
 const RegisterSchema = Yup.object().shape({
   firstName: Yup.string().required("Please enter your firstname :)"),
@@ -20,6 +21,9 @@ const RegisterSchema = Yup.object().shape({
     .min(5)
     .max(15)
     .required("Please enter your password :)"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Please re-enter your password"),
   gender: Yup.string().required("Please enter your gender :)"),
   // dateOfBirth:Yup.date().required('Please enter your birthday :)')
 });
@@ -33,6 +37,7 @@ const RegisterScreen = () => {
           lastName: null,
           email: null,
           password: null,
+          confirmPassword: null,
           gender: null,
         }}
         validationSchema={RegisterSchema}
@@ -49,63 +54,92 @@ const RegisterScreen = () => {
           <>
             <View style={styles.inputContainer}>
               <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>First Name</Text>
                 <TextInput
                   style={styles.input}
                   placeholder="First Name"
                   value={values.firstName}
                   onChangeText={handleChange("firstName")}
                   onBlur={() => setFieldTouched("firstName")}
+                  autoCapitalize="none"
                 />
                 {touched.firstName && errors.firstName && (
                   <Text style={styles.errorTxt}>{errors.firstName}</Text>
                 )}
               </View>
-
               <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>Last Name</Text>
                 <TextInput
                   style={styles.input}
                   placeholder="Last Name"
                   value={values.lastName}
                   onChangeText={handleChange("lastName")}
                   onBlur={() => setFieldTouched("lastName")}
+                  autoCapitalize="none"
                 />
                 {touched.lastName && errors.lastName && (
                   <Text style={styles.errorTxt}>{errors.lastName}</Text>
                 )}
               </View>
-
               <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>Email</Text>
                 <TextInput
                   style={styles.input}
                   placeholder="E-mail"
                   value={values.email}
                   onChangeText={handleChange("email")}
                   onBlur={() => setFieldTouched("email")}
+                  autoCapitalize="none"
                 />
                 {touched.email && errors.email && (
                   <Text style={styles.errorTxt}>{errors.email}</Text>
                 )}
               </View>
-
               <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>Password</Text>
                 <TextInput
                   style={styles.input}
                   placeholder="Password"
                   value={values.password}
                   onChangeText={handleChange("password")}
                   onBlur={() => setFieldTouched("password")}
+                  autoCapitalize="none"
                 />
                 {touched.password && errors.password && (
                   <Text style={styles.errorTxt}>{errors.password}</Text>
                 )}
               </View>
-
               <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>Confirm Password</Text>
                 <TextInput
                   style={styles.input}
+                  placeholder="Confirm Password"
+                  value={values.confirmPassword}
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={() => setFieldTouched("confirmPassword")}
+                  autoCapitalize="none"
+                />
+                {touched.confirmPassword && errors.confirmPassword && (
+                  <Text style={styles.errorTxt}>{errors.confirmPassword}</Text>
+                )}
+              </View>
+              <View style={styles.inputWrapper}>
+                <Text style={styles.fieldName}>Gender</Text>
+                <Dropdown
+                  style={styles.input}
+                  labelField="label"
+                  valueField="value"
+                  data={[
+                    { label: "Erkek", value: "Erkek" },
+                    { label: "Kadın", value: "Kadın" },
+                    {
+                      label: "Belirtmek İstemiyorum",
+                      value: "Belirtmek İstemiyorum",
+                    },
+                  ]}
                   placeholder="Gender"
                   value={values.gender}
-                  onChangeText={handleChange("gender")}
+                  onChange={(item) => handleChange("gender")(item.value)}
                   onBlur={() => setFieldTouched("gender")}
                 />
                 {touched.gender && errors.gender && (
@@ -116,9 +150,14 @@ const RegisterScreen = () => {
 
             <View style={styles.buttonContainer}>
               <TouchableOpacity
-                onPress={() => {}}
+                onPress={() => {
+                  console.log(values);
+                }}
                 disabled={!isValid}
-                style={styles.button}
+                style={[
+                  styles.button,
+                  isValid ? {} : { backgroundColor: "gray" },
+                ]}
               >
                 <Text style={styles.buttonText}>Register</Text>
               </TouchableOpacity>
@@ -160,18 +199,24 @@ const styles = StyleSheet.create({
     bottom: 0,
     padding: 10,
   },
+  fieldName: {
+    fontSize: 13,
+  },
   errorTxt: {
     color: "red",
     fontSize: 11,
   },
   button: {
+    height: 45,
     backgroundColor: "blue",
     padding: 10,
     width: "100%",
     borderRadius: 5,
     marginBottom: 10,
     alignItems: "center",
+    justifyContent: "center",
   },
+  disabled: {},
   buttonText: {
     color: "white",
     textAlign: "center",
